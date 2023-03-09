ac803865f78aa066a38673fd434f66bbb8c9e735
mohamedreda98 <mohamed.reda741@gmail.com>
2023-03-09 16:43:21 +0200

    init commit

---
+++ addons/cds_stock/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "CDS Stock Module",
+    'summary': """
+    """,
+    'description': """
+    """,
+    'author': "CDS Solutions SRL",
+    'website': "www.cdsegypt.com",
+    'contributors': [
+        'Ramadan Khalil <rkhalil1990@gmail.com>',
+    ],
+    'version': '0.1',
+    'depends': ['base', 'sale', 'product'],
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+    "pre_init_hook": None,
+    "post_init_hook": None,
+}
